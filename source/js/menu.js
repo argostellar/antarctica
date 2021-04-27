8199b2cb732ddecd5689cd5fd52df6206dd6e697
argostellar <arskud@gmail.com>
2021-04-27 23:55:10 +0700

    Вносит доработки по результатам тестирования (самостоятельно)

--- source/js/menu.js
+++ source/js/menu.js
@@ -5,17 +5,26 @@
   const header = document.querySelector(`.main-header`);
   const menuOpenBtn = document.querySelector(`.main-header__menu-btn`);
   const menuClsBtn = document.querySelector(`.main-header__cls-btn`);
+  const body = document.body;
 
-  if (header) {
+  if (header && body) {
     header.classList.remove(`main-header--no-js`);
     header.classList.add(`main-header--closed`);
 
     const onBtnOpen = () => {
       header.classList.remove(`main-header--closed`);
+      body.classList.add(`overlay`);
+      body.style.position = 'fixed';
+      body.style.top = `-${window.scrollY}px`;
     };
 
     const onBtnClose = () => {
       header.classList.add(`main-header--closed`);
+      body.classList.remove(`overlay`);
+      const scrollY = document.body.style.top;
+      body.style.position = '';
+      body.style.top = '';
+      window.scrollTo(0, parseInt(scrollY || '0') * -1);
     };
 
     if (menuOpenBtn) {
